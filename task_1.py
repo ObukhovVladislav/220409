b01b033e0ee9e70d4118e8cd65513b10317d55b9
ObukhovVladislav <vobukhov10@gmail.com>
2022-04-09 11:17:14 +0500

    task_1

--- task_1.py
+++ task_1.py
@@ -1,6 +1,6 @@
 # a, b, c = map(int, input().split(','))
 # print(a * b * c)
 
-a, b, c = [int(x) for x in input("Пожалуйста, введите цифры: ").split(',')]
+a, b, c = [int(i) for i in input("Пожалуйста, введите цифры: ").split(',')]
 d = a * b * c
 print(d)
